95d64b3728b72911c6379720f42d977310cded99
KamleshThakur123 <kkthakur93@gmail.com>
2022-12-04 10:07:16 +0530

    updated

---
+++ Dump_Data.py
@@ -0,0 +1,26 @@
+import pymongo
+import pandas as pd  
+import json 
+
+### provide the mongodb localhost url to connect mongodb with python
+
+client=pymongo.MongoClient("mongodb://localhost:27017")
+
+Data_File_Path="/config/workspace/aps_failure_training_set1.csv"
+Database_Name="aps"
+collection_Name="sensor"
+
+if __name__=="__main__":
+    df=pd.read_csv(Data_File_Path)
+    print(f"shape of the data is :{df.shape}")
+
+
+    ### convert dataframe to json so that we can dump this dataframe to mongodb
+    df.reset_index(drop=True,inplace=True)
+
+    json_records= list(json.loads(df.T.to_json()).values())
+
+    print(json_records[0])
+
+    ### insert converted json into mongodb
+    client[Database_Name][collection_Name].insert_many(json_records)
