2f84c623dcdafd9633fa34217e898dfc58b1f400
KamleshThakur123 <kkthakur93@gmail.com>
2022-12-07 09:09:18 +0530

    updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Sensor.logger import logging
 from Sensor.exception import SensorException
 from Sensor.utils import get_collection_as_dataframe
 from Sensor.Entity import config_entity
+from Sensor.Components import DataIngestion
 import sys,os
 
 
@@ -12,6 +13,8 @@ if __name__=="__main__":
           training_pipeline_config=config_entity.TrainingPipelineConfig()
           data_ingestion_config=config_entity.DataIngestionConfig(training_pipeline_config)
           print(data_ingestion_config.to_dict())
+          data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
 
