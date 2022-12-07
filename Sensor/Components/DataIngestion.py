920f85e94c62dfc67b6a927bce6d88998036691d
KamleshThakur123 <kkthakur93@gmail.com>
2022-12-07 10:25:43 +0530

    updated

--- Sensor/Components/DataIngestion.py
+++ Sensor/Components/DataIngestion.py
@@ -57,4 +57,5 @@ class DataIngestion:
         except Exception as e:
             raise SensorException(e, sys)
 
+
     
--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from Sensor.logger import logging
 from Sensor.exception import SensorException
 from Sensor.utils import get_collection_as_dataframe
 from Sensor.Entity import config_entity
-from Sensor.Components import DataIngestion
+from Sensor.Components.DataIngestion import DataIngestion
 import sys,os
 
 
 
 if __name__=="__main__":
 
+
      try:
+     
           training_pipeline_config=config_entity.TrainingPipelineConfig()
           data_ingestion_config=config_entity.DataIngestionConfig(training_pipeline_config)
           print(data_ingestion_config.to_dict())
+
+          
           data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
           print(data_ingestion.initiate_data_ingestion())
+          
      except Exception as e:
           print(e)
-
