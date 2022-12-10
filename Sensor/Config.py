01733c69f499d7086b5ac4b899c84d41e800f59c
KamleshThakur123 <kkthakur93@gmail.com>
2022-12-10 11:37:10 +0530

    updated

--- Sensor/Config.py
+++ Sensor/Config.py
@@ -9,6 +9,7 @@ import os
 class EnvironmentVariable:
     mongo_db_url:str=os.getenv("MONGO_DB_URL")
 
+
 env_var=EnvironmentVariable()
 
 
@@ -17,4 +18,5 @@ env_var=EnvironmentVariable()
 
 mongo_client=pymongo.MongoClient(env_var.mongo_db_url)
 TARGET_COLUMN = "class"
+
 
--- Sensor/Entity/artifact_entity.py
+++ Sensor/Entity/artifact_entity.py
@@ -12,8 +12,19 @@ class DataValidationArtifact:
 
 
 
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    target_encoder_path:str
+    
 
-class DataTransformationArtifact:...
-class ModelTrainerArtifact:...
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str
+    f1_train_score:float
+    f1_test_score:float 
+
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
