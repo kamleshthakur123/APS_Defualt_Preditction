ac6623657c67bf00c04ac6ac0e41fde7c684fa96
KamleshThakur123 <kkthakur93@gmail.com>
2022-12-10 13:18:39 +0530

    updated

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Sensor.Components.DataIngestion import DataIngestion
 from Sensor.Components.DataValidation import DataValidation
 from Sensor.Components.DataTransformation import DataTransformation
 from Sensor.Components.ModelTrainer import ModelTrainer
+
+from Sensor.Components.ModelEvaluation import ModelEvaluation
 import sys,os
 
 
@@ -41,6 +43,13 @@ if __name__=="__main__":
           model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
           model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
           model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+          model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+          data_ingestion_artifact=data_ingestion_artifact,
+          data_transformation_artifact=data_transformation_artifact,
+          model_trainer_artifact=model_trainer_artifact)
+          model_eval_artifact = model_eval.initiate_model_evaluation()
           
           
      except Exception as e:
