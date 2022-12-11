b8d0db3e88e668a6a9c09c5f6b53c7add167b883
kamleshthakur123 <kkthakur93@gmail.com>
2022-12-11 13:26:31 +0530

    Project completed

---
+++ Batch_prediction.py
@@ -0,0 +1,13 @@
+from Sensor.pipeline.Training_Pipeline import start_training_pipeline
+
+from Sensor.pipeline.batch_prediction import start_batch_prediction
+
+file_path="/config/workspace/aps_failure_training_set1.csv"
+print(__name__)
+if __name__=="__main__":
+     try:
+          #start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
+     except Exception as e:
+          print(e)
